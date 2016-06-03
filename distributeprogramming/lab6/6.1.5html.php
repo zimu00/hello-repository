254f3963732aefbd6f7f8dec79462d44d48984e5
zimu00 <920416015@qq.com>
2016-06-03 20:16:32 +0200

    Add files via upload
    before the result is always division,then found i forgot add break; in every case and division is the last case,so it was that result

---
+++ distributeprogramming/lab6/6.1.5html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <meta content="text/html; charset=utf-8" http-equiv="content-type">
+    <title>Esercizio 6.1.4</title>
+  </head>
+  
+  <body>
+    <form action="6.1.5.php" method="post">
+      Number1: <input type="text" name="n1"><br />
+      Number2: <input type="text" name="n2"><br />
+      <select name="operation">
+      	<option value="+">+</option>
+      	<option value="-">-</option>
+      	<option value="*">*</option>
+      	<option value="/">/</option>
+      </select>
+      <input type="reset">
+      <input type="submit">
+    </form>
+  </body>
+</html>
