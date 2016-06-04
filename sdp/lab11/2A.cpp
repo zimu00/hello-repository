7d091cb4aa424fef9d3f9654ce3293d3b5ee9118
zimu00 <920416015@qq.com>
2016-06-05 00:02:30 +0200

    Add files via upload

--- sdp/lab11/2A.cpp
+++ sdp/lab11/2A.cpp
@@ -27,25 +27,31 @@ int _tmain(int argc,LPTSTR argv[]){
 	Threads_t *thread;
 	INT count;
 	INT i;
+	INT P,C;
 
-	_stprintf (argv[1],"%d",argv[1]);
-	N = (INT)argv[3];
-	T = (INT)argv[4];
+	P=_tstoi(argv[1]);
+	C=_tstoi(argv[1]);
+	count = P + C;
+	N = _tstoi(argv[3]);
+	T = _tstoi(argv[4]);
 	_tprintf(_T("N=%d,T=%d\n"),N,T);
-	count = (INT)argv[1] + (int)argv[2];
 	_tprintf(_T("there are %d threads will be created\n"),count);
 
 	ThreadHandle = (HANDLE *)malloc(sizeof(HANDLE)*count);
 	thread = (Threads_t *)malloc(sizeof(Threads_t *)*count);
 
-	for(i=0;i<(int)argv[1];i++)
+	for(i=0;i<P;i++)
+	{
+		ThreadHandle[i] = CreateThread(0,0,(LPTHREAD_START_ROUTINE)product,&thread[i],0,&thread[i].thId);
+	}
+	for(;i<count;i++)
 	{
 		ThreadHandle[i] = CreateThread(0,0,(LPTHREAD_START_ROUTINE)consume,&thread[i],0,&thread[i].thId);
 	}
 
 
 	WaitForMultipleObjects(count,ThreadHandle,TRUE,INFINITE);
-	for(;i<count;i++)
+	for(i=0;i<count;i++)
 	{
 		CloseHandle(ThreadHandle[i]);
 	}
