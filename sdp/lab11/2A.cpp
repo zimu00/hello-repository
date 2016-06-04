651ecae2a489644a6b1b9b7df1c36249f31182c3
zimu00 <920416015@qq.com>
2016-06-05 00:49:41 +0200

    first full version

--- sdp/lab11/2A.cpp
+++ sdp/lab11/2A.cpp
@@ -7,11 +7,19 @@
 #include <tchar.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define BUFFER 200
 
 INT N;
 INT T;
+HANDLE full;
+HANDLE empty;
+HANDLE MEp;
+HANDLE MEc;
+HANDLE mutex;
+INT counter=0;
+INT products[BUFFER];
 
 typedef struct Threads
 {
@@ -37,6 +45,36 @@ int _tmain(int argc,LPTSTR argv[]){
 	_tprintf(_T("N=%d,T=%d\n"),N,T);
 	_tprintf(_T("there are %d threads will be created\n"),count);
 
+	//Create a mutex with no initial owner
+	mutex = CreateMutex(
+			NULL, // default security attributes
+			FALSE,// initially not owned
+			NULL// unnamed mutex
+		);
+	full = CreateSemaphore(
+		NULL,
+		0,
+		N,
+		NULL
+		);
+	empty = CreateSemaphore(
+		NULL,
+		N,
+		N,
+		NULL
+		);
+	MEp = CreateSemaphore(
+		NULL,
+		1,
+		1,
+		NULL
+		);
+	MEc = CreateSemaphore(
+		NULL,
+		1,
+		1,
+		NULL
+		);
 	ThreadHandle = (HANDLE *)malloc(sizeof(HANDLE)*count);
 	thread = (Threads_t *)malloc(sizeof(Threads_t *)*count);
 
@@ -60,14 +98,118 @@ int _tmain(int argc,LPTSTR argv[]){
 }
 
 DWORD WINAPI product(LPVOID param){
-	Threads_t *thread = (Threads_t *)param;
+	Threads_t *th = (Threads_t *)param;
+	//Message m;
+	srand(time(NULL));
+	int n,m;
 
+	while(1){
+	//generate random number at random time interval
+	n=rand()%(T+1);
+	Sleep(n);
+	m = rand()%10;
 
+	WaitForSingleObject(empty,INFINITE);
+	WaitForSingleObject(MEp,INFINITE);
+	//wait for mutex
+	WaitForSingleObject(mutex,INFINITE);
+	_tprintf(_T("\n%d has got the mutex\n"),th->thId);
+	//enqueue
+	products[counter%N]=m;
+	if(!ReleaseMutex(mutex))
+	{
+		_ftprintf(stderr,_T("%d Release failed.Error:%x\n"),th->thId,GetLastError());
+		return 9;
+	}
+	else 
+		_tprintf(_T("%d released the mutex\n"),th->thId);
+	}
+	//signal MEp
+	if (!ReleaseSemaphore(
+		MEp,
+		1,
+		NULL
+		))
+	{
+		_tprintf(_T("----->%d thread ReleaseMEp error:%d\n"),th->thId,GetLastError());
+	}
+	else
+	{
+		_tprintf(_T("----->%d thread have release MEp\n"),th->thId);
+	}
+	//signal full
+	if (!ReleaseSemaphore(
+		full,
+		1,
+		NULL
+		))
+	{
+		_tprintf(_T("----->%d thread Releasefull error:%d\n"),th->thId,GetLastError());
+	}
+	else
+	{
+		_tprintf(_T("----->%d thread have release full\n"),th->thId);
+	}
 	return 0;
 }
 
 DWORD WINAPI consume(LPVOID param){
-	Threads_t *thread = (Threads_t *)param;
+	Threads_t *th = (Threads_t *)param;
+
+	//Message m;
+	srand(time(NULL));
+	int n,m;
+
+	while(1){
+	//generate random number at random time interval
+	n=rand()%(T+1);
+	Sleep(n);
+	m = rand()%10;
+
+	WaitForSingleObject(full,INFINITE);
+	WaitForSingleObject(MEc,INFINITE);
+	//wait for mutex
+	WaitForSingleObject(mutex,INFINITE);
+	_tprintf(_T("\n%d consumer has got the mutex\n"),th->thId);
+	//dequeue
+	m=products[counter%N];
+	counter--;//??
+
+	if(!ReleaseMutex(mutex))
+	{
+		_ftprintf(stderr,_T("%d consumer Release failed.Error:%x\n"),th->thId,GetLastError());
+		return 9;
+	}
+	else 
+		_tprintf(_T("%d released the mutex\n"),th->thId);
+	}
+	//signal MEp
+	if (!ReleaseSemaphore(
+		MEc,
+		1,
+		NULL
+		))
+	{
+		_tprintf(_T("----->%d thread ReleaseMEp error:%d\n"),th->thId,GetLastError());
+	}
+	else
+	{
+		_tprintf(_T("----->%d thread have release MEp\n"),th->thId);
+	}
+	//signal full
+	if (!ReleaseSemaphore(
+		empty,
+		1,
+		NULL
+		))
+	{
+		_tprintf(_T("----->%d thread Releasefull error:%d\n"),th->thId,GetLastError());
+	}
+	else
+	{
+		_tprintf(_T("----->%d thread have release full\n"),th->thId);
+	}
+	_tprintf(_T("consumer %d consumes product %d"),th->thId,m);
 
 	return 0;
 }
