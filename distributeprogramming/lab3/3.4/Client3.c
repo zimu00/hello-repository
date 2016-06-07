b4030c808daf4a2e2d194c1ca6d830516c4d7b9f
zimu00 <920416015@qq.com>
2016-06-07 12:51:15 +0200

    Add files via upload

---
+++ distributeprogramming/lab3/3.4/Client3.c
@@ -0,0 +1,544 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <arpa/inet.h>
+#include "errlib.h"
+#include "sockwrap.h"
+#include <rpc/xdr.h>
+#include "type.h"
+
+#define MAXLINE 4096
+#define LISTENQ 15
+#define MAXNAME 63
+#define TIMEOUT 120
+#ifdef TRACE
+#define trace(x) x
+#else
+#define trace(x)
+#endif
+
+int sendName(int sfd,char *filename,char *ptr);
+int readControl(int sfd);
+int readSizeTime(int sfd,int *fileSize);//if int fileSize,it will only store a copy,not able to change value
+int readContent(int sfd,char *filename,int fileSize);
+int sendName_xdr(int sfd,char *filename,char *ptr,FILE *stream_socket_w);
+int readControl_xdr(int sfd,FILE *stream_socket_w);
+int readSizeTime_xdr(int sfd,int *fileSize,FILE *stream_socket_w);
+char *prog_name;
+//int daemon_proc=0;
+#define SA struct sockaddr
+
+
+int reply(int connfd){
+
+	//char buf[MAXBUFL+1]
+	return 0;
+}
+
+int main(int argc,char* argv[]){
+
+//check arguments
+	if(argc<3)
+	{
+	 printf("usage: %s <>protocol> <address> <port>\n where <protocol> is -x",prog_name);
+	 exit(1);
+	}
+	if( argc==4 )
+	{
+	 if( (argv[1][0]!='-')||(argv[1][1]!='x') )
+	 {
+		printf("usage: %s <>protocol> <address> <port>\n where <protocol> is -x",prog_name);
+		exit(1);
+		}
+	}
+char *filename=malloc(MAXNAME*sizeof(char));
+char *ptr1=malloc(MAXNAME*sizeof(char));
+prog_name=argv[0];
+int s,sfd,fileSize;
+//ssize_t nread;
+struct addrinfo hints;
+struct addrinfo *result,*rp;
+
+memset(&hints,0,sizeof(struct addrinfo));
+hints.ai_family=AF_UNSPEC;
+hints.ai_socktype=SOCK_STREAM;
+hints.ai_protocol=IPPROTO_TCP;
+hints.ai_flags=0;
+if(argc==4){
+s=getaddrinfo(argv[2],argv[3],&hints,&result);
+}
+else
+s=getaddrinfo(argv[1],argv[2],&hints,&result);
+if(s!=0)
+{
+    printf("getaddrinfo wrongly\n");
+    exit(1);
+}
+for(rp=result;rp!=NULL;rp=rp->ai_next){
+    sfd=socket(rp->ai_family,rp->ai_socktype,rp->ai_protocol);
+
+    if(sfd==-1)
+    continue;
+
+    if(connect(sfd,rp->ai_addr,rp->ai_addrlen)!=-1){
+    printf("connected!!!******\n");
+    break;}
+
+    close(sfd);}
+
+    if(rp==NULL){
+    trace(err_msg("(%s)---could not connect",prog_name));
+    exit(1);
+}
+    freeaddrinfo(result);
+    trace(err_msg("(%s)---start communication.......\n",prog_name));
+    while(1){
+    memset(ptr1,0,strlen(ptr1));//need to be cleared,ot if 1st file name is longer than 2nd,the previous tail part will be remained
+    memset(filename,0,strlen(filename));
+    trace(err_msg("(%s)---Please provide file names to be transfered or enter ctl+d TO EXIT :",prog_name));
+    int i=0;   
+    FILE *stream_socket_r = fdopen(sfd,"r");
+    if(stream_socket_r==NULL){
+    printf("error in creating stream\n");
+    
+    }
+		FILE *stream_socket_w = fdopen(sfd,"w");
+          while(1){
+		if(scanf("%c",&ptr1[i++])!=EOF){
+                //printf("%d",&ptr1[i]);
+		if((ptr1[i-1]=='\n')||(ptr1[i-1]=='\r'))
+			break;
+                	}
+ else{
+        ptr1[0]='\0';
+        strcat(ptr1, "QUIT\r\n");
+        puts(ptr1);	
+	if(argc==4){
+		message m;
+		XDR xdrs_w;
+		
+		memset(&m,0,sizeof(message));
+		m.tag = 2;//should be after memset
+		//initialize xdr stream
+    		
+    		xdrstdio_create(&xdrs_w,stream_socket_w,XDR_ENCODE);
+
+    		//send to the server
+    		if(!xdr_message(&xdrs_w,&m))
+    		//if (!xdr_tagtype (&xdrs_w, &m))
+    		{
+    			printf("Error in sending xdr quit messages!\n");
+    			xdr_destroy(&xdrs_w);
+    			fclose(stream_socket_w);
+    			fclose(stream_socket_r);
+    			close(sfd);
+    			return 0;
+    		}
+    		fflush(stream_socket_w);
+    		printf("I have sent quit information to server!m.tag=%d\n",m.tag);
+    		xdr_destroy(&xdrs_w);
+    		fclose(stream_socket_w);
+    		fclose(stream_socket_r);
+    		}
+      else{
+	int n=writen(sfd,ptr1,strlen(ptr1));
+       if(n!=strlen(ptr1))
+          printf("errrrrrrr1\n");
+      }
+          
+printf("I'm going to close,strlen(ptr)=%lu",strlen(ptr1));
+close(sfd);
+
+return 0;}
+  }
+   
+       trace(err_msg("(%s)----in process of sending file name to server.....",prog_name));
+       if(argc==3){
+       if(sendName(sfd,filename,ptr1)!=0)
+         break;
+       if(readControl(sfd)<0)
+         break;
+       if(!(2==readSizeTime(sfd,&fileSize)))
+         break;
+       }
+       else{
+       //if(sendName_xdr(sfd,filename,ptr1,stream_socket_w)!=0)
+         //---------------send name----------------
+         				int i=0;
+				message m,m_out;
+		XDR xdrs_w,xdrs_r;
+				memset(&m,0,sizeof(message));
+			
+	m.tag = GET;//GET = 0
+				
+        filename[i]='\0';
+        strcat(filename,ptr1);
+        
+        //to find the position where there's no \r or \n,to add \0 for simplicity to add \r\n together
+       i=strlen(filename)-1;//needs to -1,otw it is \0,of course not ='\n' and '\r'
+	while(1){
+             if(filename[i]!='\n'&&filename[i]!='\r')
+               break;
+             --i;
+             
+                }
+                filename[++i]='\0';
+   
+   	for(i=0;i<strlen(filename);i++)
+	printf("%d ",filename[i]);
+        //char *filename=(char*)malloc((strlen(full)-5)*sizeof(char));//including '\0'
+        //strncpy(filename,full+4,strlen(full)-6);
+        printf("Filename: %s", filename);
+    printf("TVB :-)\n\n");
+    
+    
+    //initialize xdr stream
+    //FILE *stream_socket_w = fdopen(sfd,"w");
+    if(stream_socket_w ==NULL){
+    	err_sys ("(%s)error - fdopen() failed",prog_name);
+    	break;
+    	}
+    xdrstdio_create(&xdrs_w,stream_socket_w,XDR_ENCODE);
+    
+    //send to the server
+    m.message_u.filename = strdup(filename);
+    xdr_message(&xdrs_w,&m);
+    fflush(stream_socket_w);
+
+    trace(err_msg("(%s)---has sent to the server!",prog_name));
+    xdr_destroy(&xdrs_w);
+         //-----------------------------------------
+         
+          printf("waiting for control\n!");
+        //if(readControl_xdr(sfd,stream_socket_r)<0)
+        
+     	
+	
+	
+	printf("waiting for control message");
+    	xdrstdio_create(&xdrs_r,stream_socket_r,XDR_DECODE);
+	printf("I have created control stream");
+	memset(&m_out,0,sizeof(message));
+	//if (!xdr_tagtype (&xdrs_r, &m_out.tag))///why stuck here?
+	//	printf("error in receiving\n");
+	xdr_message(&xdrs_r,&m_out);
+    		/*if(!xdr_message(&xdrs_r,&m_out))
+
+    		{
+    			printf("Error in receiving xdr  messages!\n");
+    			xdr_destroy(&xdrs_r);
+    			fclose(stream_socket_r);
+    			close(sfd);
+    			return -1;
+    		}*/
+    		
+    		printf("has received control message\n");
+    		int return_value;
+    		 FILE *fp;
+    		switch (m_out.tag){
+    		case OK:
+    			return_value =1;
+    			printf("received ok\n!");
+    			fileSize = m_out.message_u.fdata.contents.contents_len;
+    			printf("the filesize is %u\n",fileSize);
+    			u_int time =m_out.message_u.fdata.last_mod_time;
+    			printf("the time is %u\n",time);
+    			/********content********/
+			char *cbuf;
+    cbuf = 
+strdup(m_out.message_u.fdata.contents.contents_val);
+//writing into file
+FILE *fp=fopen(filename,"wb");
+    if(NULL==fp){
+      printf("Error opening file");
+      exit(-1);
+    }
+
+int f=fwrite(cbuf,1,fileSize,fp);
+     if(f<fileSize)
+     printf("error\n"); 
+
+    trace(err_msg("(%s)---download finished!",prog_name));
+
+			fclose(fp);
+			/***********************/
+    			break;
+    		case ERR:
+    			printf("received error\n");
+    			return_value =-1;
+    			break;
+    		
+    		}
+    		if(return_value == -1)
+    		break;
+    		/*-----------------------------------------------*/
+       
+       // if(!(2==readSizeTime_xdr(sfd,&fileSize,stream_socket_r)))
+         //break;
+       }
+       printf("ha,I'm in\n");
+       if(argc == 3)
+       if(!(readContent(sfd,filename,fileSize)==1)){
+         
+         break;}
+       
+       
+   //memset(&ptr,0,strlen(ptr));???????
+
+          
+}
+printf("I'm going to close");
+close(sfd);
+free(ptr1);
+//if(argc==4){
+//fclose(stream_socket_w);
+//fclose(stream_socket_r);}???
+return 0;
+}
+
+int sendName(int sfd,char *filename,char *ptr){
+	//char *ptr=malloc(MAXNAME*sizeof(char));
+	int i=0;
+        char *full=malloc(MAXNAME*sizeof(char));
+       // memset(full,0,MAXNAME*sizeof(char));
+        full[i++]='G';
+        full[i++]='E';
+        full[i++]='T';
+        full[i++]=' ';
+        full[i]='\0';
+        //strncpy(ptr,"GET ",ptr);
+        strcat(full,ptr);
+        
+        //to find the position where there's no \r or \n,to add \0 for simplicity to add \r\n together
+       i=strlen(full)-1;//needs to -1,otw it is \0,of course not ='\n' and '\r'
+	while(1){
+             if(full[i]!='\n'&&full[i]!='\r')
+               break;
+             --i;
+             
+                }	
+	full[++i]='\0';
+	strcat(full, "\r\n"); /* this adds \r\n to the message that have to be sent  */
+        printf("full is:%s",full);
+   
+        //char *filename=(char*)malloc((strlen(full)-5)*sizeof(char));//including '\0'
+        strncpy(filename,full+4,strlen(full)-6);
+        printf("Filename: %s", filename);
+    printf("TVB :-)\n\n");
+    
+    //send to the server
+    if(write(sfd,full,strlen(full))!=strlen(full)){
+    perror("partial/failed write\n");
+    exit(-1);
+   
+    }
+    trace(err_msg("(%s)---has sent to the server!",prog_name));
+    free(full);
+    //free(ptr);cz it will delete ptr1 in main
+return 0;
+}
+
+int sendName_xdr(int sfd,char *filename,char *ptr,FILE *stream_socket_w){
+	
+
+    
+    //free(ptr);cz it will delete ptr1 in main
+    //free(full);
+return 0;
+}
+
+int readControl(int sfd){
+	char *tbuf=malloc(MAXNAME*sizeof(char));
+        fd_set socket_set;
+	struct timeval timeout;
+	
+	/* set connection timeout */
+	FD_ZERO(&socket_set); 
+	FD_SET(sfd, &socket_set);
+	timeout.tv_sec = TIMEOUT;
+	timeout.tv_usec = 0;
+        int n;
+	
+	if( (n = Select(FD_SETSIZE, &socket_set, NULL, NULL, &timeout)) == -1)
+	{
+		printf("select() failed\n");
+		return -1;
+	}
+	if(n > 0)
+	{
+
+//can't use readline
+	    //Readline(sfd,tbuf,MAXNAME);
+	   readline_unbuffered(sfd,tbuf,MAXNAME);
+	    sprintf(tbuf,"%s",tbuf);
+	    trace(err_msg("(%s)---received control message from server is:%s",prog_name,tbuf));
+	    if(strcmp(tbuf,"+OK\r\n")==0){
+	      free(tbuf);
+	      return 1;}
+	    else{
+	      free(tbuf);
+	      return -1;}}
+       else{
+           free(tbuf);
+           trace(err_msg("(%s)---not receive response after %d seconds",prog_name,TIMEOUT));
+           return -1;
+      }
+}
+
+int readControl_xdr(int sfd,FILE *stream_socket_r){
+	
+    		return 0;
+}
+
+
+int readSizeTime(int sfd,int *fileSize){
+	 message m;
+	 XDR xdrs_r;
+	 memset(&m,0,sizeof(message));
+        fd_set socket_set;
+	struct timeval timeout;
+	uint32_t value,ts1,ts2;
+	/* set connection timeout */
+	FD_ZERO(&socket_set); 
+	FD_SET(sfd, &socket_set);
+	timeout.tv_sec = TIMEOUT;
+	timeout.tv_usec = 0;
+        int nn;
+	
+	if( (nn = Select(FD_SETSIZE, &socket_set, NULL, NULL, &timeout)) == -1)
+	{
+		printf("select() failed\n");
+		return -1;
+	}
+	if(nn > 0)
+	{
+
+    
+    int n=readn(sfd,&value,sizeof(uint32_t));
+    if(n<0)
+    trace(err_msg("(%s)---error in reading files",prog_name));
+    ts1=ntohl(value);
+    *fileSize=(int)ts1;
+    //rbuf[n]='\0';  
+    trace(err_msg("(%s)---The file size is:%d,*fileSize=%d",prog_name,(int)ts1,*fileSize));
+    //*fileSize=ts1;
+    //trace(err_msg("(%s)---The file size is:%u,ts1=%u",prog_name,ntohl(value1),ts1));  
+    n=readn(sfd,&value,sizeof(uint32_t));
+    ts2=ntohl(value);
+    trace(err_msg("(%s)---The file modified time is:%d",prog_name,(int)ts2));
+    //trace(err_msg("(%s)---The file modified time is:%u,ts2=%u",prog_name,ntohl(value2),ts2));
+   // while(2){}
+    return 2;}
+    else{
+           trace(err_msg("(%s)---not receive response after %d seconds",prog_name,TIMEOUT));
+           return -1;
+      }
+    }
+int readSizeTime_xdr(int sfd,int *fileSize,FILE *stream_socket_w){
+fd_set socket_set;
+	struct timeval timeout;
+	uint32_t value,ts1,ts2;
+	/* set connection timeout */
+	FD_ZERO(&socket_set); 
+	FD_SET(sfd, &socket_set);
+	timeout.tv_sec = TIMEOUT;
+	timeout.tv_usec = 0;
+        int nn;
+	
+	if( (nn = Select(FD_SETSIZE, &socket_set, NULL, NULL, &timeout)) == -1)
+	{
+		printf("select() failed\n");
+		return -1;
+	}
+	if(nn > 0)
+	{
+
+	//xdrstdio_create(&xdrs_r,stream_socket_r,XDR_DECODE);
+	
+    int n=readn(sfd,&value,sizeof(uint32_t));
+    if(n<0)
+    trace(err_msg("(%s)---error in reading files",prog_name));
+    ts1=ntohl(value);
+    *fileSize=(int)ts1;
+    //rbuf[n]='\0';  
+    trace(err_msg("(%s)---The file size is:%d,*fileSize=%d",prog_name,(int)ts1,*fileSize));
+    //*fileSize=ts1;
+    //trace(err_msg("(%s)---The file size is:%u,ts1=%u",prog_name,ntohl(value1),ts1));  
+    n=readn(sfd,&value,sizeof(uint32_t));
+    ts2=ntohl(value);
+    trace(err_msg("(%s)---The file modified time is:%d",prog_name,(int)ts2));
+    //trace(err_msg("(%s)---The file modified time is:%u,ts2=%u",prog_name,ntohl(value2),ts2));
+   // while(2){}
+    return 2;}
+    else{
+           trace(err_msg("(%s)---not receive response after %d seconds",prog_name,TIMEOUT));
+           return -1;
+      }
+}
+
+int readContent(int sfd,char *filename,int fileSize){
+ //create a file where to store data
+ 	char *cbuf=malloc(MAXLINE*sizeof(char));
+       printf("dasdaa\n");
+        int n=1;
+    FILE *fp=fopen(filename,"wb");
+    if(NULL==fp){
+      printf("Error opening file");
+      exit(-1);
+    }
+        fd_set socket_set;
+	struct timeval timeout;
+	int nn;
+	/* set connection timeout */
+	FD_ZERO(&socket_set); 
+	FD_SET(sfd, &socket_set);
+	timeout.tv_sec = TIMEOUT;
+	timeout.tv_usec = 0;
+	
+	if( (nn = Select(FD_SETSIZE, &socket_set, NULL, NULL, &timeout)) == -1)
+	{
+		printf("select() failed\n");
+		return -1;
+	}
+	if(nn > 0)
+	{
+        trace(err_msg("(%s)----I'm going to download....fileSize=%d",prog_name,fileSize));
+    //fileSize=55330;
+    int bRemained=fileSize;
+    int rBytes,i=0;
+    while(bRemained>0)
+    {i++;
+    if(bRemained>=MAXLINE)
+      {rBytes=MAXLINE;}
+    else
+      {rBytes=bRemained;}
+    //printf("\ni=%d",i);
+   
+
+// printf("rBytes=%d,bRemained=%d\n",rBytes,bRemained);
+    n=readn(sfd,cbuf,rBytes);//cz readline_un stopps when there's \n,so one line once
+    //n=readline_unbuffered(sfd,cbuf,rBytes);
+   // if(n!=rBytes)
+     // trace (err_msg("(%s) n=%d error - readn() failed", prog_name,n));
+    //cbuf[n]='\0';  
+
+ // printf("Bytes received %d\n",n);
+    int f=0;
+    //while(f<n)
+    f=fwrite(cbuf,1,n,fp);
+     if(f<n)
+     printf("error\n"); 
+    //memset(cbuf,0,n);
+    bRemained-=n;
+    }trace(err_msg("(%s)---download finished!,times=%d,rBytes=%d",prog_name,i,rBytes));
+return 1;}
+else{
+           trace(err_msg("(%s)---not receive response after %d seconds",prog_name,TIMEOUT));
+           return -1;
+      }
+}
