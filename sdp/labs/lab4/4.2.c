0f6feb6d42ba97033212085d0f372158c3a7c819
zimu00 <920416015@qq.com>
2016-06-24 10:29:53 -0700

    Add files via upload

--- sdp/labs/lab4/4.2.c
+++ sdp/labs/lab4/4.2.c
@@ -20,6 +20,7 @@ typedef struct{
 void *swap(void *param);
 void checkBoundary(thread *th);
 
+thread *th;
 int finish=0;
 int n;
 int *buf;
@@ -29,7 +30,7 @@ sem_t se,*se1;
 
 int main(int argc,char *argv[]){
 
-if(argc!=3)
+if(argc!=2)
 {
 	printf("paramete error\n");
 	exit(-1);
@@ -42,14 +43,14 @@ int *pi;
 struct stat stat_buf;
 char c,*paddr;
 int len;
-thread *th;
 
 pthread_mutex_init(&lock,NULL);
 fp = open(argv[1],O_RDWR);
 fstat(fp,&stat_buf);
-len = stat_buf.st_size;
+len = stat_buf.st_size/(sizeof(int));
 double x = log10(len);
 n = ceil(x);
+printf("%d,%d\n",len,n);
 pth = malloc(n*sizeof(pthread_t));
 pi = malloc(n*sizeof(int));
 th = malloc(n*sizeof(thread));
@@ -60,21 +61,29 @@ flag = (int *)calloc(sizeof(int),n/2);
 paddr = mmap( (caddr_t)0,len,PROT_READ | PROT_WRITE,MAP_SHARED,(int )fp,0 );
 buf = (int *)paddr;
 sem_init(&se,0,0);
+close(fp);
 
-for(;i<n;i++)
+printf("len=%d the original array is :\n",len);
+for(;i<len;i++)
+printf("%d ",buf[i]);
+
+for(i=0;i<n;i++)
 {
 	th[i].left = i*len/n;
-	if(n>=(i+1)*len/n)
+	printf("%d ",th[i].left);
+	sleep(3);
+	if(len>=(i+1)*len/n)
 	th[i].right = th[i].left +len/n;
 	else
-	th[i].right = n;
+	th[i].right = len;
 	
 	th[i].id = i;
 	pth[i] = pthread_create(&pth[i],NULL,swap,&th[i]);
 }
-
+sleep(1000000);
 pthread_exit(NULL);
-return 0;
+//sleep(10000000000);
+//return 0;
 }
 
 void *swap(void *param){
@@ -83,22 +92,23 @@ thread *th = (thread *)param;
 int i,j;
 int tmp;
 
-printf("\nI'm thread  before sorting%d\n",th->id);
+pthread_detach(pthread_self());
+printf("\nI'm thread %d before sorting\n",th->id);
 for(i = th->left;i<th->right;i++){
 	printf("%d ",buf[i]);
 }
 
 while(1){
 for(i=th->left;i<th->right;i++){
-	for(j=i;j<th->right-i-1;j++){
-		if(buf[i]>buf[i+1]){
-			tmp = buf[i];
-			buf[i]=buf[i+1];
-			buf[i+1]=tmp;
+	for(j=th->left;j<th->right;j++){
+		if(buf[j]>buf[j+1]){
+			tmp = buf[j];
+			buf[j]=buf[j+1];
+			buf[j+1]=tmp;
 		}	
 	}
 }
-printf("thread %d finishes swapping\n",th->id);
+printf("\nthread %d finishes swapping,%d,%d\n",th->id,th->left,th->right);
 for(i = th->left;i<th->right;i++){
 	printf("%d ",buf[i]);
 }
@@ -107,31 +117,32 @@ pthread_mutex_lock(&lock);
 if(th->id==0)
 flag[th->id]++;
 else{
-flag[th->id%2]++;
-flag[th->id%2+1]++;
+flag[th->id/2]++;
+flag[th->id/2+1]++;
 }
 pthread_mutex_unlock(&lock);
 
-if(flag[th->id%2]==2){
+if(flag[th->id/2]==2){
 	sem_post(&se);
+	printf("\n thread%d sent semaphore\n",th->id);
 
 	if(th->id==n-1){
 //check
 	checkBoundary(th);
 	}
 
-	sem_wait(&se1[th->id%2]);
+	sem_wait(&se1[th->id/2]);
 
 	}
 	
-	if(flag[th->id%2+1]){
+	if(flag[th->id/2+1]==2){
 //sem_post(se[th->id/2]);
 	sem_post(&se);
 	if(th->id==n-1){
 //check
 	checkBoundary(th);
 	}
-	sem_wait(&se1[th->id%2+1]);
+	sem_wait(&se1[th->id/2+1]);
 
 	}
 }
@@ -140,10 +151,22 @@ if(flag[th->id%2]==2){
 void checkBoundary(thread *th){
 
 	int i;
-	int tmp;	
+	int tmp;
+	int finish=0;	
+	int time=0;
 	
 	while(1){
+	if(time==n-1 && finish==n-1)
+	{
+		printf("all are sorted\n");
+		for(i=0;i<th[n-1].left;i++)
+			printf("%d ",buf[i]);
+		exit(0);	
+	}
+	
 	sem_wait(&se);
+	time++;
+	
 	pthread_mutex_lock(&lock);
 	for(i=0;i<n-1;i++){
 	
@@ -153,12 +176,14 @@ void checkBoundary(thread *th){
 	}
 	pthread_mutex_unlock(&lock);
 	
-	if(buf[i]>buf[i+1])
+	if(buf[th[i].right]>buf[th[i+1].left])
 	{
-		tmp = buf[i];
-		buf[i] = buf[i+1];
-		buf[i+1] = tmp;
+		tmp = buf[th[i].right];
+		buf[th[i].right] = buf[th[i+1].left];
+		buf[th[i+1].left] = tmp;
 	}
+	else
+		finish++;
 	
 	if(th->id==i-1){
 		sem_post(&se1[i]);
