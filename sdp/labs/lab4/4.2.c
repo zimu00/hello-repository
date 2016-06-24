10ea304f58cb973103873ef9e74a30ab8a29434a
zimu00 <920416015@qq.com>
2016-06-24 07:34:08 -0700

    Create 4.2.c

---
+++ sdp/labs/lab4/4.2.c
@@ -0,0 +1,156 @@
+//only last thread checks
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <math.h>
+
+typedef struct{
+	int left;
+	int right;
+	int id;
+}thread;
+
+void *swap(void *param);
+void checkBoundary(thread *th);
+
+int finish=0;
+int n;
+int *buf;
+pthread_mutex_t lock;
+int *flag;
+sem_t se,*se1;
+
+int main(int argc,char *argv[]){
+
+if(argc!=3)
+{
+	printf("paramete error\n");
+	exit(-1);
+}
+
+int i=0;
+pthread_t *pth;
+FILE *fp;
+int *pi;
+struct stat stat_buf;
+char c,*paddr;
+int len;
+thread *th;
+
+pthread_mutex_init(&lock);
+fp = fopen(argv[1],"r");
+fstat(fd,&stat_buf);
+len = stat_buf.st_size;
+n = ceiling(log10(len));
+pth = malloc(n*sizeof(pthread_t));
+pi = malloc(n*sizeof(int));
+th = malloc(n*sizeof(thread));
+//se = malloc(n/2*sizeof(sem_t));
+se1 = malloc((n-1)*sizeof(sem_t));
+flag = (int *)calloc(sizeof(int),n/2);
+
+paddr = mmap( (caddr_t)0,len,PROT_READ | PROT_WRITE,MAP_SHARED,fp,0 );
+buf = (int *)paddr;
+sem_init(se,0,0);
+
+for(;i<n;i++)
+{
+	th[i].left = i*len/n;
+	if(n>=(i+1)*len/n)
+	th[i].right = th[i].left +len/n;
+	else
+	th[i].right = n;
+	th[i] = i;
+	pth[i] = pthread_create(th,NULL,swap,&th[i]);
+}
+
+pthread_exit(NULL);
+return 0;
+}
+
+void *swap(void *param){
+
+thread *th = (thread *)param;
+int i,j;
+int tmp;
+
+while(1){
+for(i=th->left;i<th->right;i++){
+	for(j=i;j<th->right-i-1;j++){
+		printf("");
+		if(buf[i]>buf[i+1]){
+			tmp = buf[i];
+			buf[i]=buf[i+1];
+			buf[i+1]=tmp;
+		}	
+	}
+}
+printf("thread %d finishes swapping\n",th->id);
+
+pthread_mutex_lock(&lock);
+if(th->id==0)
+flag[th->id]++;
+else{
+flag[th->id%2]++;
+flag[th->id%2+1]++;
+}
+pthread_mutex_unlock(&lock);
+
+if(flag[th->id%2]==2){
+	sem_post[&se];
+
+	if(th->id==n-1){
+//check
+	checkBoundary();
+	}
+
+	sem_wait(&se1[th->id%2]);
+
+	}
+	
+	if(flag[th->id%2+1]){
+//sem_post(se[th->id/2]);
+	sem_post[&se];
+	if(th->id==n-1){
+//check
+	checkBoundary(th);
+	}
+	sem_wait(&se1[th->id%2+1]);
+
+	}
+}
+}
+
+void checkBoundary(thread *th){
+
+	int i;
+	int tmp;	
+	
+	while(1){
+	sem_wait(se);
+	pthread_mutex_lock(&lock);
+	for(i=0;i<n-1;i++){
+	
+		if(flag[i]==2)
+			break;
+	
+	}
+	pthread_mutex_unlock(&lock);
+	
+	if(buf[i]>buf[i+1])
+	{
+		tmp = buf[i];
+		buf[i] = buf[i+1];
+		buf[i+1] = tmp;
+	}
+	
+	if(th.id==i-1){
+		sem_post(i);
+		swap(th);
+	}
+	sem_post(i);
+	sem_post(i);
+	
+	}
+}
+	
